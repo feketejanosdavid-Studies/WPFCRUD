5c02f23e2f8e88b5aad438b735ef6a0b7b2a6595
feketejanosdavid <janosdavidfekete@gmail.com>
2024-05-08 09:03:14 +0200

    kezdetek

---
+++ WPFCRUD/Windowadd.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFCRUD
+{
+    /// <summary>
+    /// Interaction logic for Windowadd.xaml
+    /// </summary>
+    public partial class Windowadd : Window
+    {
+        public Windowadd()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
